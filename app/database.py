c20ed33ab6c1761dbd6188f82842e6a23cefc356
rauliesan <raullsdev@gmail.com>
2025-08-05 13:07:36 +0200

    Coding in database.py and models.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://usuario:contraseña@host:5432/nombre_base"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@db.ixmujviepzpsxkycnfhv.supabase.co:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, Table, DateTime
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, Boolean, Table, DateTime
 from sqlalchemy.orm import relationship
-from datetime import datetime
 from .database import Base
 
 # Tabla de usuarios
@@ -9,14 +8,18 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
-    password_hash = Column(String, nullable=False)
+    password = Column(String, nullable=False)
 
-    tasks_user = relationship("Task", back_populates="user")
+    tasks_user = relationship("Task", back_populates="user") # Relación entre el usuario y la tarea por ejemplo para acceder a todas las tareas de un usuario usuario.tasks_user
 
 class Task(Base):
     __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False) # Nombre de la tarea
+    id_user = Column(Integer, ForeignKey(User.id), nullable=False)
+    text = Column(String, nullable=False) # Texto informativo sobre de que trata la tarea
+    state = Column(Boolean, nullable=False) # El estado de la tarea (false) por defecto porque cuando se crea no está realizada
+    # Añadir como actualización la fecha de creación
 
     user = relationship("User", back_populates="task")
