3036f5d441b8ad8f4b1d7b98bcc3f0e2bc546b37
rauliesan <raullsdev@gmail.com>
2025-08-05 18:24:58 +0200

    Coding in main.py

---
+++ app/main.py
@@ -0,0 +1,86 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.models import User, Task, Base
+from app.database import SessionLocal, engine
+
+# Crear las tablas en la base de datos
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+## Habilita CORS en FastAPI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Función para obtener la sesión de la base de datos
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/create_user/")
+def create_user(username: str, password: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == username).first()
+    if user:
+        raise HTTPException(status_code=400, detail="Error, an user has this username")
+        #return "Error, an user has this username"
+    user = User(username=username, password=password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+@app.get("/log_in/")
+def log_in(username: str, password: str, db: Session= Depends(get_db)):
+    user = db.query(User).filter(User.username==username, User.password==password).first()
+    if user:
+        return "You are log in"
+    else:
+        raise HTTPException(status_code=400, detail="Error, this password or username is incorrect")
+
+@app.post("/create_task/")
+def create_task(name: str, text: str, state: bool, user: int, db: Session= Depends(get_db)):
+    task = db.query(Task).filter(Task.name==name, user==user).first()
+    if task:
+        raise HTTPException(status_code=400, detail="Error, there is a task with the same name")
+    task = Task(name=name, text=text, state=state, id_user=user)
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+    return task
+
+@app.post("/edit_task/")
+def edit_task(id: int, name: str, text: str, state: bool, user: int, db: Session= Depends(get_db)):
+    task = db.query(Task).filter(Task.id==id).first()
+    if not task:
+        raise HTTPException(status_code=400, detail="Error, I can't find this task")
+    task.name= name
+    task.text= text
+    task.state= state
+    db.commit()
+    db.refresh(task)
+    return task
+
+@app.post("/edit_state/")
+def edit_state(id: int, db: Session= Depends(get_db)):
+    task = db.query(Task).filter(Task.id==id).first()
+    if not task:
+        raise HTTPException(status_code=400, detail="Error, I can't find this task")
+    if task.state==True:
+        task.state=False
+    else:
+        task.state=True
+    db.commit()
+    db.refresh(task)
+    return task
--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text, ForeignKey, Boolean, Table, DateTime
 from sqlalchemy.orm import relationship
-from .database import Base
+from app.database import Base
 
 # Tabla de usuarios
 class User(Base):
@@ -16,7 +16,7 @@ class Task(Base):
     __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False) # Nombre de la tarea
+    name = Column(String, unique=True, nullable=False) # Nombre de la tarea
     id_user = Column(Integer, ForeignKey(User.id), nullable=False)
     text = Column(String, nullable=False) # Texto informativo sobre de que trata la tarea
     state = Column(Boolean, nullable=False) # El estado de la tarea (false) por defecto porque cuando se crea no está realizada
