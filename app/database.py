b8500a42e328b12f28a7de5cbe2a8100fc322100
rauliesan <raullsdev@gmail.com>
2025-08-08 12:06:03 +0200

    Changing the database url

--- app/database.py
+++ app/database.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@db.ixmujviepzpsxkycnfhv.supabase.co:5432/postgres"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@db.ixmujviepzpsxkycnfhv.supabase.co:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.ixmujviepzpsxkycnfhv:password@aws-0-eu-north-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
