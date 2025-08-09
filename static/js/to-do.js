f34a96a0ded5dc42dffe7e98c047011a4e6fe73f
rauliesan <raullsdev@gmail.com>
2025-08-09 02:55:06 +0200

    Coding on the principal page

--- static/js/to-do.js
+++ static/js/to-do.js
@@ -1,8 +1,9 @@
 let user = localStorage.getItem('user');
 let message = localStorage.getItem('message');
 
+/* Hacer una pantalla de carga y una función que sea nada más cargar la página que si el usuario es null salga un error y no se pueda acceder a la página o que devuelva a la página de inicio de sesión */
+
 function toDo() {
-    alert(user); /* Quitar en el futuro */
 
     fetch('https://todopython-b750.onrender.com/get_toDo_tasks/?user=' + user)
         .then(response => {
@@ -22,14 +23,18 @@ function toDo() {
                         <p class="text-body">${task.text}</p>
                     </div>
                     <div class="card-actions">
-                        <button class="delete" onclick="deleteTask(${task.id})"> 
+                        <div class="half">
+                            <button class="delete" onclick="deleteTask(${task.id}, 'toDo')"> 
                             <svg viewBox="0 0 448 512" class="svgIcon">
                                 <path
                                     d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z">
                                 </path>
                             </svg>
                         </button>
-                        <button class="card-button done" onclick="changeState(${task.id})">Done</button>
+                        </div>
+                        <div class="half">
+                            <button class="card-button done" onclick="changeState(${task.id}, 'toDo')">Done</button>
+                        </div>
                     </div>
                 </div>
             `;
@@ -39,7 +44,6 @@ function toDo() {
 }
 
 function done(){
-    alert(user);
 
     fetch('https://todopython-b750.onrender.com/get_done_tasks/?user='+user)
     .then(response => {
@@ -60,14 +64,18 @@ function done(){
                         <p class="text-body">${task.text}</p>
                     </div>
                     <div class="card-actions">
-                        <button class="delete" onclick="deleteTask(${task.id})"> 
+                        <div class="half">
+                            <button class="delete" onclick="deleteTask(${task.id}, 'done')"> 
                             <svg viewBox="0 0 448 512" class="svgIcon">
                                 <path
                                     d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z">
                                 </path>
                             </svg>
                         </button>
-                        <button class="card-button done" onclick="changeState(${task.id})">To Do</button>
+                        </div>
+                        <div class="half">
+                            <button class="card-button done" onclick="changeState(${task.id}, 'done')">To Do</button>
+                        </div>
                     </div>
                 </div>
             `;
@@ -77,14 +85,18 @@ function done(){
 
 }
 
-function changeState(taskId){
-    alert('Done task ' + taskId);
+function changeState(taskId, state){
     fetch('https://todopython-b750.onrender.com/edit_state/?id='+taskId, {
         method: 'POST'
     })
     .then(response =>{
         if(!response.ok){
             throw new Error('Error, The state couldnt been changed')
+        }
+        if(state=='toDo'){
+            toDo();
+        } else{
+            done();
         }
     })
 }
@@ -93,8 +105,7 @@ function createTask(){
     
 }
 
-function deleteTask(taskId){
-    alert('Delete task ' + taskId);
+function deleteTask(taskId, state){
     fetch('https://todopython-b750.onrender.com/delete_task/?id='+taskId, {
         method: 'POST'
     })
@@ -102,10 +113,15 @@ function deleteTask(taskId){
         if(!response.ok){
             throw new Error('Error, The task couldnt been deleted')
         }
+        if(state=='toDo'){
+            toDo(); 
+        } else{
+            done();
+        }
     })
 }
 
 function logOut() {
-    user = undefined;
+    localStorage.removeItem('user');
     window.location.href = "index.html";
 }
